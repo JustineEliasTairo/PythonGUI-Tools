3f8d1a864e526f440cb349b4c18154628a3ba433
Justin <justine.tairo@gmail.com>
2024-07-30 10:16:56 +0200

    mport all Modules to be used in the Application

---
+++ ImageApp.py
@@ -0,0 +1,20 @@
+
+# Import all Modules
+
+import os
+from datetime import datetime
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication,\
+                            QWidget,\
+                            QPushButton,\
+                            QVBoxLayout,\
+                            QHBoxLayout,\
+                            QListWidget,\
+                            QComboBox,\
+                            QFileDialog,\
+                            QLabel
+from PyQt5.QtGui import QPixmap     # To load the image into PyQt
+from PIL import Image,\
+                ImageFilter,\
+                ImageEnhance
+
