6f9853c96a07a083dca8f823b5ec006f48519830
Justin <justine.tairo@gmail.com>
2024-07-30 10:24:28 +0200

    Create the main entry of the Program, Create a skeleton of a main class for the App, Set initial Window

--- ImageApp.py
+++ ImageApp.py
@@ -18,3 +18,49 @@ from PIL import Image,\
                 ImageFilter,\
                 ImageEnhance
 
+
+
+# Creating a Class that inherits a super class "QWidget"
+class ImageApp(QWidget):
+    
+    def __init__(self):
+        super().__init__()
+        
+        self.setWindowTitle("Image Editor App");
+        self.resize(900,720);
+
+
+
+    # End def __init__(self): 
+    
+    
+    
+        
+    
+
+
+# End class ImageApp():
+
+
+
+
+
+
+
+
+
+# Start the program
+if __name__ in "__main__":
+
+    # Main App Obj and settings
+    # ---------------------------
+    app = QApplication([]);
+    main_win = ImageApp();
+    
+
+    # Show and Run the App
+    # ---------------------------
+    main_win.show();
+    app.exec_();
+
+# End if __name__ in "__main__":
