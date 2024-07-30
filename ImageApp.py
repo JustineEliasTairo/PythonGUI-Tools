31860903167de15b3686618c52d7cd98990db5d3
Justin <justine.tairo@gmail.com>
2024-07-30 10:28:22 +0200

    Create all objects of the app - Widgets and Layouts

--- ImageApp.py
+++ ImageApp.py
@@ -28,6 +28,64 @@ class ImageApp(QWidget):
         
         self.setWindowTitle("Image Editor App");
         self.resize(900,720);
+        
+        # Create all Obj/Widgets here
+        # ---------------------------
+        # - Design Here 
+        # - Handle Events Here
+        
+        self.btn_folder      = QPushButton("Folder");
+        self.file_list       = QListWidget();
+        
+        self.btn_left        = QPushButton("Left");
+        self.btn_right       = QPushButton("Right");
+        self.btn_mirror      = QPushButton("Mirror");
+        self.btn_sharpness   = QPushButton("Sharpness");
+        self.btn_gray        = QPushButton("B/W");
+        self.btn_saturation  = QPushButton("Saturation");
+        self.btn_contrast    = QPushButton("Contrast");
+        self.btn_blur        = QPushButton("Blur");
+        
+        # Dropdown box
+        self.filter_box = QComboBox();
+        self.filter_box.addItem("Original");
+        self.filter_box.addItem("Left");
+        self.filter_box.addItem("Right");
+        self.filter_box.addItem("Mirror");
+        self.filter_box.addItem("Sharpness");
+        self.filter_box.addItem("B/W");
+        self.filter_box.addItem("Saturation");
+        self.filter_box.addItem("Contrast");
+        self.filter_box.addItem("Blur");
+        
+        self.pic_box = QLabel("Image space!");
+        
+        # Main Layout (Hor)
+        master_Layout = QHBoxLayout();
+        
+        colmn1 = QVBoxLayout();
+        colmn2 = QVBoxLayout();
+        
+        colmn1.addWidget(self.btn_folder);
+        colmn1.addWidget(self.file_list);
+        colmn1.addWidget(self.filter_box);
+        
+        colmn1.addWidget(self.btn_left);
+        colmn1.addWidget(self.btn_right);
+        colmn1.addWidget(self.btn_mirror);
+        colmn1.addWidget(self.btn_sharpness);
+        colmn1.addWidget(self.btn_gray);
+        colmn1.addWidget(self.btn_saturation);
+        colmn1.addWidget(self.btn_contrast);
+        colmn1.addWidget(self.btn_blur);
+        
+        colmn2.addWidget(self.pic_box);
+        
+        master_Layout.addLayout(colmn1,stretch=20);
+        master_Layout.addLayout(colmn2,stretch=80);
+        
+        # Set Master Layout to the Main Window
+        self.setLayout(master_Layout);
 
 
 
